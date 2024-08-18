09071b988724da13624252ce2aa8f39534d47804
LVL_UP <126270067+udithdoddapaneni@users.noreply.github.com>
2024-08-18 23:40:02 +0530

    Update API.py

--- API.py
+++ API.py
@@ -3,16 +3,23 @@ from Main import MODEL
 from PIL import Image
 from pydantic import BaseModel
 from io import BytesIO
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins (change this to specific origins for production)
+    allow_credentials=True,
+    allow_methods=["GET", "POST"],  # Allow all methods (GET, POST, etc.)
+    allow_headers=["*"],  # Allow all headers
+)
 
 @app.post("/evaluate/")
 async def get(image: UploadFile = File()):
     image_data = await image.read()
     image = Image.open(BytesIO(image_data))
-
     result = MODEL.predict(image)
-
+    print(result)
     return {"result": result}
